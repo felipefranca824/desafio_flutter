9303eceb7302cc4da54ca6aa5ef3c14f9cbeb146
Felipe <felipefranca824@gmail.com>
2021-03-01 22:28:13 -0300

    Criado o Layout principal

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:desafio_galoes/widgets/item_list.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Desafio'),
+        centerTitle: true,
+      ),
+      body: 
+        Column(
+          children: [
+            TextField(
+        decoration: InputDecoration(
+          hintText: 'Volume do Galão',
+          
+        ),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          hintText: 'Quantidade de garrafas',
+          
+        ),
+      ),
+      Expanded(
+        child: ListView.builder(
+        itemCount: 4,
+        itemBuilder: _buildList,
+      )
+      ),
+          ],
+        ),
+      
+    );
+
+  }
+
+  Widget _buildList(context, index){
+    print(index);
+      return Row(
+        children: [
+          Text('Garrafa ${index + 1}'),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                
+              ),
+            )
+          )
+        ],
+      );
+    }
+}
